aac7d1c489c989396bbcd7f8f0c0dd178cc11f3a
h-varmazyar <hossein.varmazyar@yahoo.com>
2023-07-01 21:13:18 +0330

    fix(chipmunk): insert bulk candles every minute

--- services/chipmunk/internal/app/candles/workers/candleReader.go
+++ services/chipmunk/internal/app/candles/workers/candleReader.go
@@ -2,6 +2,7 @@ package workers
 
 import (
 	"context"
+	"fmt"
 	"github.com/golang/protobuf/proto"
 	"github.com/h-varmazyar/Gate/pkg/amqpext"
 	"github.com/h-varmazyar/Gate/pkg/mapper"
@@ -12,6 +13,7 @@ import (
 	indicatorsPkg "github.com/h-varmazyar/Gate/services/chipmunk/internal/pkg/indicators"
 	log "github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
+	"time"
 )
 
 type CandleReader struct {
@@ -19,6 +21,7 @@ type CandleReader struct {
 	configs    *Configs
 	queue      *amqpext.Queue
 	indicators []indicatorsPkg.Indicator
+	insertChan chan *entity.Candle
 }
 
 func NewCandleReaderWorker(_ context.Context, db repository.CandleRepository, configs *Configs) (*CandleReader, error) {
@@ -26,11 +29,15 @@ func NewCandleReaderWorker(_ context.Context, db repository.CandleRepository, co
 	if err != nil {
 		return nil, err
 	}
-	return &CandleReader{
-		db:      db,
-		configs: configs,
-		queue:   ohlcQueue,
-	}, nil
+	reader := &CandleReader{
+		db:         db,
+		configs:    configs,
+		queue:      ohlcQueue,
+		insertChan: make(chan *entity.Candle, 1000),
+	}
+	go reader.handleInsert()
+
+	return reader, nil
 }
 
 func (w *CandleReader) Start(indicators []indicatorsPkg.Indicator) {
@@ -62,17 +69,47 @@ func (w *CandleReader) handle(delivery amqp.Delivery) {
 		tmp := new(entity.Candle)
 		mapper.Struct(candle, tmp)
 		localCandles = append(localCandles, tmp)
+
+		w.insertChan <- tmp
 	}
 
 	for _, indicator := range w.indicators {
 		indicator.Update(localCandles)
 	}
 
-	if err := w.db.BulkInsert(localCandles); err != nil {
-		log.WithError(err).Errorf("failed to save rateLimiters")
-	}
 	for _, candle := range localCandles {
 		buffer.CandleBuffer.Push(candle)
 	}
 	_ = delivery.Ack(false)
+}
+
+func (w *CandleReader) handleInsert() {
+	key := ""
+	candleMap := make(map[string]*entity.Candle)
+	lastInsert := time.Now()
+	for candle := range w.insertChan {
+		key = fmt.Sprintf("%v%v%v", candle.MarketID, candle.ResolutionID, candle.Time.Unix())
+		candleMap[key] = candle
+		if time.Now().Sub(lastInsert) > time.Minute {
+			err := w.insert(candleMap)
+			if err != nil {
+				log.WithError(err).Errorf("failed to insert candles")
+				continue
+			}
+			candleMap = make(map[string]*entity.Candle)
+			lastInsert = time.Now()
+		}
+	}
+}
+
+func (w *CandleReader) insert(candleMap map[string]*entity.Candle) error {
+	candles := make([]*entity.Candle, 0)
+	for _, candle := range candleMap {
+		candles = append(candles, candle)
+	}
+
+	if err := w.db.BulkInsert(candles); err != nil {
+		return err
+	}
+	return nil
 }
